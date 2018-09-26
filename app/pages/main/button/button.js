56542c06dcf362a4eb8d2d9d8e573a6b56e695cc
cavy24 <vakulenko2410@mail.ru>
2018-09-26 14:53:40 +0300

    Added buttons: link to catalog and forms submission

---
+++ app/pages/main/button/button.js
@@ -0,0 +1,8 @@
+function goCatalog() {
+  let btn_catalog = document.getElementById('btn_catalog');
+  btn_catalog.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.location.href = "https://ru.stackoverflow.com/questions/646449/%D0%9A%D0%B0%D0%BA-%D0%BF%D0%B5%D1%80%D0%B5%D0%B9%D1%82%D0%B8-%D0%BD%D0%B0-%D0%B4%D1%80%D1%83%D0%B3%D1%83%D1%8E-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83";
+  })
+};
+//https://ru.stackoverflow.com/questions/646449/%D0%9A%D0%B0%D0%BA-%D0%BF%D0%B5%D1%80%D0%B5%D0%B9%D1%82%D0%B8-%D0%BD%D0%B0-%D0%B4%D1%80%D1%83%D0%B3%D1%83%D1%8E-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%83
